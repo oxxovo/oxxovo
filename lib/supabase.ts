9011f5e5ab336b702691830fa706d0cd2578af5d
Thomas Kim <tkckusa@gmail.com>
2026-05-09 12:41:59 -0700

    fix: use publishable key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qrnkovokjmimagrwjebs.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFybmtvdm9ram1pbWFncndqZWJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTc1NzQsImV4cCI6MjA5MzgzMzU3NH0.9Hsq-_6DD9zwrYqj7Fqu5Ji48B1YzIEk2M3J9T6wHWQ'
+const supabaseAnonKey = 'sb_publishable_jqaYD8CyZLZLK3mpCPjHMQ_f79qUrjl'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
